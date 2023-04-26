15ab120882726652f121144d4edfa8731ba6a537
Felipe Souza <felipedb91@gmail.com>
2023-04-26 16:46:26 -0300

    feat: create service interfaces

--- src/main/java/dev/fsantana/diojpa/service/IAvaliacaoFisicaService.java
+++ src/main/java/dev/fsantana/diojpa/service/IAvaliacaoFisicaService.java
@@ -1,5 +1,15 @@
 package dev.fsantana.diojpa.service;
 
+import java.util.List;
+
+import dev.fsantana.diojpa.dto.AvaliacaoFisicaInput;
+import dev.fsantana.diojpa.model.AvaliacaoFisica;
+
 public interface IAvaliacaoFisicaService {
 
+    AvaliacaoFisica create(AvaliacaoFisicaInput data);
+
+    AvaliacaoFisica get(Long id);
+
+    List<AvaliacaoFisica> getAll();
 }
