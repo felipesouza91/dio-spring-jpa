692910d7df5270134e5331184c2b2e63722441da
Felipe Souza <felipedb91@gmail.com>
2023-04-26 22:51:14 -0300

    feat: create controller class

--- src/main/java/dev/fsantana/diojpa/controller/MatriculaController.java
+++ src/main/java/dev/fsantana/diojpa/controller/MatriculaController.java
@@ -1,5 +1,10 @@
 package dev.fsantana.diojpa.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/matricula")
 public class MatriculaController {
-  
+
 }
